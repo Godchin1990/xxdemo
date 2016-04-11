1b0388ede82b94734c67afc587d9a27bacf5cd39
godchin <380580655@qq.com>
2016-04-11 15:29:29 +0800

    dd

--- app/src/main/java/www/godchin/demo/MainActivity.java
+++ app/src/main/java/www/godchin/demo/MainActivity.java
@@ -1,9 +1,13 @@
 package www.godchin.demo;
 
 import android.content.IntentFilter;
+import android.graphics.Rect;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,4 +26,57 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
         registerReceiver(wifiReceiver, intentFilter);
     }
+    //region 自定义弹出菜单
+
+    boolean isShowing = false;
+    private View v;
+
+    private void animation(View v) {
+        if (!isShowing) {
+            v.setVisibility(View.VISIBLE);
+            v.setAnimation(AnimationUtils.loadAnimation(this, R.anim.translate_popipwindow_enter));
+        } else {
+            v.setAnimation(AnimationUtils.loadAnimation(this, R.anim.translate_popipwindow_exit));
+            v.setVisibility(View.GONE);
+        }
+        isShowing = !isShowing;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        isInIgnoredView(event);
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * 判断是否点击在指定区域，如果不是则隐藏对话框
+     */
+    private boolean isInIgnoredView(MotionEvent ev) {
+        Rect rect = new Rect();
+        getRectInScreen(v, rect);
+        if (!rect.contains((int) ev.getX(), (int) ev.getY()) && isShowing) {
+            animation(v);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the boundary of a view in screen coordinates.
+     * 类似 Windows SDK 中的 GetWindowRect + ClientToScreen
+     *
+     * @param v
+     * @param r The result.
+     */
+    private void getRectInScreen(View v, Rect r) {
+        final int w = v.getWidth();
+        final int h = v.getHeight();
+        r.left = v.getLeft();
+        r.top = v.getTop();
+        r.right = r.left + w;
+        r.bottom = r.top + h;
+    }
+    //endregion
 }
+
+
